26f5dd7978e49c32e33288254a9637fef2b36443
René Schünemann <rene.schuenemann@sap.com>
2022-05-24 15:43:47 +0200

    uset env vars

--- hack/integration-test.sh
+++ hack/integration-test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # SPDX-FileCopyrightText: 2022 "SAP SE or an SAP affiliate company and Gardener contributors"
 #
@@ -13,8 +13,6 @@ then
     apk add --no-cache --no-progress git
 fi
 
-
-
 PROJECT_ROOT="$(dirname $0)/.."
 TARGET_CLUSTER="laas-integration-test"
 TARGET_CLUSTER_PROVIDER="gcp"
@@ -22,11 +20,8 @@ LAAS_REPOSITORY="eu.gcr.io/sap-se-gcr-k8s-private/cnudie/gardener/development"
 REPO_AUTH_URL="https://eu.gcr.io"
 REPO_CTX_BASE_URL="eu.gcr.io/sap-se-gcr-k8s-private"
 
-LAAS_VERSION="$(cat "${PROJECT_ROOT}/VERSION")"
-pushd "${PROJECT_ROOT}" > /dev/null 2>&1
-LAAS_VERSION="$VERSION-$(git rev-parse HEAD)"
-popd > /dev/null 2>&1
-
+unset EFFECTIVE_VERSION
+LAAS_VERSION="$(${PROJECT_ROOT}/hack/get-version.sh)"
 
 export PROJECT_ROOT
 export TARGET_CLUSTER
