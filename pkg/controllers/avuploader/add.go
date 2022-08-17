bd38a438add7f819ad29b43e3015aa0230163461
enrico-kaack-comp <enrico.kaack@sap.com>
2022-08-17 10:36:49 +0200

    start avuploader only if config exitsts

--- pkg/controllers/avuploader/add.go
+++ pkg/controllers/avuploader/add.go
@@ -18,6 +18,11 @@ import (
 // AddControllerToManager adds the AVUploader controller to the manager
 func AddControllerToManager(ctx context.Context, logger logr.Logger, mgr manager.Manager, config *coreconfig.LandscaperServiceConfiguration) error {
 	log := logger.WithName("AVUploader")
+	if config.AvailabilityMonitoring.AvailabilityServiceConfiguration == nil {
+		log.Info("AvailabilityServiceConfiguration missing, not starting AVUploader")
+		return nil
+	}
+
 	ctrl, err := NewController(log, mgr.GetClient(), mgr.GetScheme(), config)
 	if err != nil {
 		return err
