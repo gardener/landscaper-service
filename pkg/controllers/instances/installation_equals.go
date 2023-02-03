969a8c440f4f5ab7458e1545f060195522570277
René Schünemann <rene.schuenemann@sap.com>
2023-02-03 10:00:42 +0100

    more refactoring

--- pkg/controllers/instances/installation_equals.go
+++ pkg/controllers/instances/installation_equals.go
@@ -14,43 +14,32 @@ import (
 
 // InstallationSpecDeepEquals test whether two installation specs are deeply equal.
 func InstallationSpecDeepEquals(specA, specB *lsv1alpha1.InstallationSpec) bool {
-	testImportDataMappings := func(specA, specB *lsv1alpha1.InstallationSpec) bool {
-		for k, v := range specA.ImportDataMappings {
-			if _, ok := specB.ImportDataMappings[k]; !ok {
-				return false
-			}
-
-			var importDataMappingA interface{}
-			if err := json.Unmarshal(v.RawMessage, &importDataMappingA); err != nil {
-				return false
-			}
-
-			var importDataMappingB interface{}
-			if err := json.Unmarshal(specB.ImportDataMappings[k].RawMessage, &importDataMappingB); err != nil {
-				return false
-			}
-
-			if !reflect.DeepEqual(importDataMappingA, importDataMappingB) {
-				return false
-			}
-
-			delete(specA.ImportDataMappings, k)
-			delete(specB.ImportDataMappings, k)
-		}
-
-		return true
-	}
-
 	if len(specA.ImportDataMappings) != len(specB.ImportDataMappings) {
 		return false
 	}
 
-	if !testImportDataMappings(specA, specB) {
-		return false
+	for k, v := range specA.ImportDataMappings {
+		if _, ok := specB.ImportDataMappings[k]; !ok {
+			return false
+		}
+
+		var importDataMappingA interface{}
+		if err := json.Unmarshal(v.RawMessage, &importDataMappingA); err != nil {
+			return false
+		}
+
+		var importDataMappingB interface{}
+		if err := json.Unmarshal(specB.ImportDataMappings[k].RawMessage, &importDataMappingB); err != nil {
+			return false
+		}
+
+		if !reflect.DeepEqual(importDataMappingA, importDataMappingB) {
+			return false
+		}
 	}
-	if !testImportDataMappings(specB, specA) {
-		return false
-	}
+
+	specA.ImportDataMappings = nil
+	specB.ImportDataMappings = nil
 
 	return reflect.DeepEqual(specA, specB)
 }
