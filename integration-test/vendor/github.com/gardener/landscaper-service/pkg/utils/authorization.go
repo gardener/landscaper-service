c08d5c6aab3c3cd4b738f8044756ade3e1459db4
enrico-kaack-comp <enrico.kaack@sap.com>
2023-06-20 10:10:56 +0200

    vendor

---
+++ integration-test/vendor/github.com/gardener/landscaper-service/pkg/utils/authorization.go
@@ -0,0 +1,58 @@
+// SPDX-FileCopyrightText: 2023 "SAP SE or an SAP affiliate company and Gardener contributors"
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package utils
+
+import (
+	"context"
+	"fmt"
+
+	kutils "github.com/gardener/landscaper/controller-utils/pkg/kubernetes"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func CreateRoleIfNotExistOrUpdate(ctx context.Context, name string, namespace string, rules []rbacv1.PolicyRule, client client.Client) error {
+	role := &rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+
+	_, err := kutils.CreateOrUpdate(ctx, client, role, func() error {
+		role.Rules = rules
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed ensuring role %s: %w", role.Name, err)
+	}
+
+	return nil
+}
+
+func CreateRoleBindingIfNotExistOrUpdate(ctx context.Context, name string, namespace string, roleName string, client client.Client) error {
+	//create role binding
+	roleBinding := &rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+	}
+	_, err := kutils.CreateOrUpdate(ctx, client, roleBinding, func() error {
+		roleBinding.RoleRef = rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     roleName,
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed ensuring role binding %s: %w", roleBinding.Name, err)
+	}
+
+	return nil
+}
