a1ae93900a85c82b7f00b522716c57300c1dc9b3
René Schünemann <rene.schuenemann@sap.com>
2024-01-26 16:19:00 +0100

    fix integration test script (run-int-tests)

--- hack/integration-test.py
+++ hack/integration-test.py
@@ -31,14 +31,10 @@ test_cluster_kubeconfig = utils.get_shoot_adminkubeconfig(test_cluster, gardener
 print(f"Getting kubeconfig for {hosting_cluster}")
 hosting_cluster_kubeconfig = utils.get_shoot_adminkubeconfig(hosting_cluster, gardener_cluster, "garden-laas")
 
-print(f"Getting credentials for {repo_ctx_base_url}")
-cr_conf = model.container_registry.find_config(repo_ctx_base_url, oa.Privileges.READONLY)
-
 with (
     utils.TempFileAuto(prefix="test_cluster_kubeconfig_") as test_cluster_kubeconfig_temp_file,
     utils.TempFileAuto(prefix="hosting_cluster_kubeconfig_") as hosting_cluster_kubeconfig_temp_file,
     utils.TempFileAuto(prefix="gardener_cluster_kubeconfig_") as gardener_cluster_kubeconfig_temp_file,
-    utils.TempFileAuto(prefix="registry_auth_", suffix=".json") as registry_temp_file
 ):
     test_cluster_kubeconfig_temp_file.write(test_cluster_kubeconfig)
     test_cluster_kubeconfig_path = test_cluster_kubeconfig_temp_file.switch()
@@ -48,9 +44,6 @@ with (
 
     gardener_cluster_kubeconfig_temp_file.write(yaml.safe_dump(gardener_cluster_kubeconfig.kubeconfig()))
     gardener_cluster_kubeconfig_path = gardener_cluster_kubeconfig_temp_file.switch()
-
-    registry_temp_file.write(json.dumps(auths))
-    registry_secrets_path = registry_temp_file.switch()
 
     command = ["go", "run", "./pkg/main.go",
                 "--test-purpose", test_purpose,
